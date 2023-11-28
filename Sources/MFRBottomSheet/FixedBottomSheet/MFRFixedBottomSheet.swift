ad2a92d0fdf5c07b4f208eea60d1c86bbfd4d888
marcofebriano <febrianomarco@gmail.com>
2023-11-28 15:43:34 +0700

    add fixed bottom sheet (not perfect)

---
+++ Sources/MFRBottomSheet/FixedBottomSheet/MFRFixedBottomSheet.swift
@@ -0,0 +1,156 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marco Febriano Ramadhani on 28/11/23.
+//
+
+import Foundation
+import UIKit
+
+open class MFRFixedBottomSheet: MFRBaseBottomSheet {
+    
+    private let overlayBackground: MFRBaseBottomSheetOverlay
+    private var bottomConstraint: NSLayoutConstraint?
+    private var isDismissable: Bool
+    
+    public lazy var containerView: UIView = {
+        var view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.accessibilityIdentifier = "containerView"
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    public init(dismissable: Bool, overlayBackground: MFRBaseBottomSheetOverlay = MFRDefaultOverlayBottomSheet()) {
+        self.overlayBackground = overlayBackground
+        self.isDismissable = dismissable
+        super.init()
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("MFRSlidingBottomSheet shouldn't be used via xib. You can use xib for the MFRSlidingBottomSheet's containerView.")
+    }
+    
+    open override func initialSetup() {
+        super.initialSetup()
+        overlayBackground.didTapOverlay = { [weak self] in
+            self?.dismiss(animated: true, withInfo: nil, completion: nil)
+        }
+        setupLayout()
+    }
+    
+    open override func show(fromView parentView: UIView, animated: Bool, completion: MFRVoidClosure?) {
+        if self.superview != parentView {
+            self.translatesAutoresizingMaskIntoConstraints = false
+            parentView.addSubview(self)
+        }
+        overlayBackground.initialSetup(self, overView: parentView, animated: animated)
+        overlayBackground.alphaPercentageChanged(self, toPercentage: 1, animated: animated)
+        addContraints(inSuperView: parentView)
+    }
+    
+    open override func dismiss(animated: Bool, withInfo: Any?, completion: MFRVoidClosure?) {
+        overlayBackground.bottomSheet(self, willDismiss: withInfo, animated: animated)
+        delegate?.bottomSheet(self, willDismiss: withInfo, animated: animated)
+        animateContainerHeight(self.frame.height, completion: { [weak self] in
+            self?.removeBottomSheet(withInfo: withInfo, animated: animated)
+        })
+    }
+    
+    func setupLayout() {
+        bottomSheetView.addSubview(containerView)
+        
+        NSLayoutConstraint.activate([
+            shadowView.topAnchor.constraint(equalTo: self.topAnchor),
+            shadowView.leftAnchor.constraint(equalTo: self.leftAnchor),
+            shadowView.rightAnchor.constraint(equalTo: self.rightAnchor),
+            shadowView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50),
+            
+            bottomSheetView.topAnchor.constraint(equalTo: shadowView.topAnchor, constant: 8),
+            bottomSheetView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            bottomSheetView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+            containerView.leadingAnchor.constraint(equalTo: bottomSheetView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: bottomSheetView.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: bottomSheetView.bottomAnchor),
+            containerView.topAnchor.constraint(equalTo: bottomSheetView.topAnchor, constant: 8)
+        ])
+        let bottomSheetToBottom = bottomSheetView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        bottomSheetToBottom.priority = .defaultHigh
+        bottomSheetToBottom.isActive = true
+    }
+    
+    private func removeBottomSheet(withInfo: Any?, animated: Bool) {
+        self.removeFromSuperview()
+        self.delegate?.bottomSheet(self, didDismiss: withInfo, animated: animated)
+    }
+}
+
+extension MFRFixedBottomSheet {
+    private func addContraints(inSuperView: UIView) {
+        let dict = ["cardView": self]
+        inSuperView.addConstraints(
+            NSLayoutConstraint.constraints(
+                withVisualFormat: "H:|-0-[cardView]-0-|",
+                options: [],
+                metrics: nil,
+                views: dict
+            )
+        )
+        let bottomAnchor = self.bottomAnchor.constraint(equalTo: inSuperView.bottomAnchor)
+        bottomAnchor.isActive = true
+        self.topAnchor.constraint(greaterThanOrEqualTo: inSuperView.safeAreaLayoutGuide.topAnchor).isActive = true
+        
+        inSuperView.layoutIfNeeded()
+        bottomConstraint = bottomAnchor
+        
+        let beforeConstant = bottomAnchor.constant
+        bottomAnchor.constant = self.frame.height
+        inSuperView.layoutIfNeeded()
+        bottomAnchor.constant = beforeConstant
+        UIView.animate(
+            withDuration: 0.7,
+            delay: 0,
+            usingSpringWithDamping: 0.7,
+            initialSpringVelocity: 0,
+            options: [.allowUserInteraction, .allowUserInteraction]) { [weak inSuperView] in
+                guard let strSuperView = inSuperView else { return }
+                strSuperView.layoutIfNeeded()
+            }
+    }
+    
+    private func animatePresentBottomSheet() {
+        /// this animation to update the constant of the bottom constraint to superView to be 0.
+        /// that means, the card will show
+        
+        UIView.animate(
+            withDuration: 0.7,
+            delay: 0,
+            usingSpringWithDamping: 0.7,
+            initialSpringVelocity: 0,
+            options: [.allowUserInteraction, .allowUserInteraction]) { [weak self] in
+                guard let self = self else { return }
+                self.bottomConstraint?.constant = 0
+                self.superview?.layoutIfNeeded()
+                self.layoutIfNeeded()
+        }
+    }
+    
+    private func animateContainerHeight(_ height: CGFloat, completion: (() -> Void)? = nil) {
+        UIView.animate(
+            withDuration: 0.7,
+            delay: 0,
+            usingSpringWithDamping: 0.7,
+            initialSpringVelocity: 0,
+            options: [.allowUserInteraction, .beginFromCurrentState]) { [weak self] in
+                guard let self = self else { return }
+                self.bottomConstraint?.constant = height
+                self.superview?.layoutIfNeeded()
+                self.layoutIfNeeded()
+            } completion: { complete in
+                guard complete else { return }
+                completion?()
+            }
+    }
+}
